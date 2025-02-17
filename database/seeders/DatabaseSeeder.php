6b7fe9db1550b300330f345ea740de0b52ef6c88
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2025-02-17 11:01:50 +0700

    menambahkan komentar

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,9 +10,9 @@ class DatabaseSeeder extends Seeder
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run(): void //Metode run() dalam konteks migrasi dan seeding di Laravel digunakan                                                       untuk menjalankan logika tertentu saat migrasi atau seeding dilakukan
     {
-        $this->call(TaskListSeeder::class);
-        $this->call(TaskSeeder::class);
+        $this->call(TaskListSeeder::class);// digunakan dalam seeder utama di Laravel untuk memanggil dan                                               menjalankan TaskListSeeder, yang akan mengisi tabel task_lists dengan data yang telah didefinisikan dalam TaskListSeeder.
+        $this->call(TaskSeeder::class);//digunakan untuk memanggil TaskSeeder di dalam seeder utama di Laravel.
     }
 }
