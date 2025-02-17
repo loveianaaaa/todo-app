b9eaffe81c7b476faf6f3c57591dd9a4b8e60071
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2025-02-17 10:21:16 +0700

    menambahkan komentar

--- database/migrations/2025_01_30_024704_create_task_lists_table.php
+++ database/migrations/2025_01_30_024704_create_task_lists_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;//Ini adalah bagian dari Laravel (f
 use Illuminate\Database\Schema\Blueprint;//Ini digunakan untuk mengimpor kelas Blueprint dari namespace Illuminate\Database\Schema di Laravel. 
 use Illuminate\Support\Facades\Schema;//Ini digunakan untuk mengimpor Schema dari Illuminate\Support\Facades di Laravel. Schema adalah kelas yang sangat penting dalam Laravel untuk mengelola struktur database melalui migrasi
 
-return new class extends Migration
+return new class extends Migration //Ini adalah contoh penggunaan anonymous class atau kelas anonim di PHP, yang di Laravel                               digunakan dalam konteks migrasi database.
 {
     /**
      * Run the migrations.
