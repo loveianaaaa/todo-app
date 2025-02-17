6195c451ffdeee59ac29c2b51fd5f724cd531a44
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2025-02-17 10:45:58 +0700

    menambahkan komentar

--- database/migrations/2025_01_30_024848_create_tasks_table.php
+++ database/migrations/2025_01_30_024848_create_tasks_table.php
@@ -13,13 +13,13 @@ return new class extends Migration//Merupakan cara penulisan migrasi dengan meng
     {
         Schema::create('tasks', function (Blueprint $table) { // digunakan untuk membuat tabel baru dalam database.                                          Ini adalah bagian dari migrasi di Laravel yang memungkinkan kamu untuk mendefinisikan struktur tabel                                             database dengan menggunakan schema builder.
             $table->id();
-            $table->string('name');
-            $table->string('description')->nullable();
-            $table->boolean('is_completed')->default(false);
-            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');
-            $table->timestamps();
+            $table->string('name');//digunakan untuk mendefinisikan kolom dalam tabel yang bertipe string. 
+            $table->string('description')->nullable();//digunakan untuk membuat kolom description dengan                                                     tipe string yang dapat menyimpan nilai teks dan boleh kosong (NULL
+            $table->boolean('is_completed')->default(false);//digunakan dalam migrasi di Laravel untuk mendefinisikan                                     kolom bertipe boolean yang akan menyimpan nilai true atau false, dengan nilai default false. Biasanya,
+            $table->enum('priority', ['low', 'medium', 'high'])->default('medium');// di Laravel digunakan untuk                                          mendefinisikan kolom priority yang bertipe ENUM
+            $table->timestamps();//digunakan untuk menambahkan dua kolom created_at dan updated_at ke tabel
 
-            $table->foreignId('list_id')->constrained('task_lists', 'id')->onDelete('cascade');
+            $table->foreignId('list_id')->constrained('task_lists', 'id')->onDelete('cascade');                                                                                                                                      //digunakan untuk menambahkan kolom list_id yang merupakan foreign key yang menghubungkan                                                     tabel yang sedang dibuat dengan tabel lain, dalam hal ini dengan tabel task_lists.
         });
     }
 
