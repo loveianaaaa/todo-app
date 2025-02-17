346eff15c3e80ba3cfadeb00dd95be4f609fd440
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2025-02-17 10:38:34 +0700

    menambahkan komentar

--- database/migrations/2025_01_30_024848_create_tasks_table.php
+++ database/migrations/2025_01_30_024848_create_tasks_table.php
@@ -4,14 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+return new class extends Migration//Merupakan cara penulisan migrasi dengan menggunakan kelas anonim                                                (anonymous class) di Laravel, yang diperkenalkan di PHP 7.0. 
 {
     /**
      * Run the migrations.
      */
-    public function up(): void
+    public function up(): void //digunakan untuk menentukan perubahan yang akan diterapkan pada                                                         struktur database ketika migrasi dijalankan.
     {
-        Schema::create('tasks', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) { // digunakan untuk membuat tabel baru dalam database.                                          Ini adalah bagian dari migrasi di Laravel yang memungkinkan kamu untuk mendefinisikan struktur tabel                                             database dengan menggunakan schema builder.
             $table->id();
             $table->string('name');
             $table->string('description')->nullable();
