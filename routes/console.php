4d7e664230b4439c3cf4280bb51e1e502a94e61b
loveianaaaa <2223602.loveiana@smkn-2sbg.sch.id>
2025-02-18 09:42:06 +0700

    menambahkan komentar

--- routes/console.php
+++ routes/console.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
+Artisan::command('inspire', function () { // Ini mendefinisikan perintah Artisan kustom yang bernama inspire.                                           Ketika kamu menjalankan php artisan inspire dari terminal, kode di dalam closure (fungsi) ini akan dijalankan.
+    $this->comment(Inspiring::quote());//mengacu pada penggunaan class Inspiring untuk mendapatkan kutipan                                               inspiratif yang kemudian ditampilkan di terminal menggunakan metode comment dalam Artisan command di Laravel.
 })->purpose('Display an inspiring quote')->hourly();
+//Ini memberikan deskripsi atau tujuan dari task yang dijadwalkan. 
+//->hourly():Ini adalah penjadwalan task untuk dijalankan setiap jam
